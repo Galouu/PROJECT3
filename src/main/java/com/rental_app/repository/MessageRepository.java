3c64f6213d0055145217a2a768bd0e6d482d01be
Galaad Michaud <gannette971@hotmail.fr>
2024-11-30 06:59:23 +0100

    fix: Remove unnecessary files and update .gitignore

--- src/main/java/com/rental_app/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rental_app.repository;
-
-import com.rental_app.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
