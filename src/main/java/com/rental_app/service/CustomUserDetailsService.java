f4ec840602f098f130451cbfdcae3750433455bf
Galaad Michaud <gannette971@hotmail.fr>
2024-11-30 07:27:41 +0100

    feat: Add CustomUserDetailsService for JWT authentication

---
+++ src/main/java/com/rental_app/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.rental_app.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.rental_app.model.User;
+import com.rental_app.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String identifier) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(identifier)
+                .or(() -> userRepository.findByName(identifier))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with identifier: " + identifier));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .build();
+    }
+}
